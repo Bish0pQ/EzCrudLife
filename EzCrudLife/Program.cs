d88c7d65cd7f35023705b19e8dd048d940176e4f
Tommy Versantvoort <tommy@alfa-zet.be>
2022-12-15 17:57:57 +0100

    🎉 Initial commit

---
+++ EzCrudLife/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// TODO: Get table information and column information
+// TODO: Generate models
+// TODO: Generate repositories
+// TODO: Generate services
+// TODO: Generate controller endpoints
+// TODO: Generate UI
+// TODO: Options
